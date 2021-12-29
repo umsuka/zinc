1b013ed70a9f44639f901c546531e0c790c52faa
Prabhat Sharma <hi.prabhat@gmail.com>
2021-12-29 05:04:54 -0800

    add pyroscope profiling

--- cmd/zinc/main.go
+++ cmd/zinc/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	zerolog "github.com/rs/zerolog/log"
 
 	"github.com/prabhatsharma/zinc/pkg/routes"
 	"github.com/prabhatsharma/zinc/pkg/zutils"
+
+	"github.com/pyroscope-io/client/pyroscope"
 )
 
 func main() {
+
+	serverAddress := os.Getenv("PYROSCOPE_SERVER_ADDRESS")
+	if serverAddress == "" {
+		serverAddress = "http://localhost:4040"
+	}
+	pyroscope.Start(pyroscope.Config{
+		ApplicationName: "zinc.app",
+		ServerAddress:   serverAddress,
+		Logger:          pyroscope.StandardLogger,
+	})
 
 	err := godotenv.Load()
 	if err != nil {
