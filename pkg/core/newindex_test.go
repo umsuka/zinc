53b45a89c909b228a3d203b927f2b0272283046e
hengfeiyang <hengfei.yang@gmail.com>
2022-05-13 00:05:50 +0800

    add tests

--- pkg/core/newindex_test.go
+++ pkg/core/newindex_test.go
@@ -18,31 +18,129 @@ package core
 import (
 	"testing"
 
+	"github.com/blugelabs/bluge/analysis"
+	"github.com/blugelabs/bluge/analysis/analyzer"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewIndex(t *testing.T) {
-	t.Run("test new index storage - disk", func(t *testing.T) {
-		indexName := "TestNewIndex.index_1"
-		index, err := NewIndex(indexName, "disk", nil)
+	type args struct {
+		name                  string
+		storageType           string
+		defaultSearchAnalyzer *analysis.Analyzer
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "normal",
+			args: args{
+				name:                  "TestNewIndex.index_1",
+				storageType:           "disk",
+				defaultSearchAnalyzer: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "underline prefix",
+			args: args{
+				name:                  "_TestNewIndex.index_2",
+				storageType:           "disk",
+				defaultSearchAnalyzer: nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "with analyzer",
+			args: args{
+				name:                  "TestNewIndex.index_3",
+				storageType:           "disk",
+				defaultSearchAnalyzer: analyzer.NewStandardAnalyzer(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "empty",
+			args: args{
+				name:                  "",
+				storageType:           "disk",
+				defaultSearchAnalyzer: nil,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewIndex(tt.args.name, tt.args.storageType, tt.args.defaultSearchAnalyzer)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.Nil(t, err)
+			assert.NotNil(t, got)
+
+			err = StoreIndex(got)
+			assert.Nil(t, err)
+
+			t.Run("cleanup", func(t *testing.T) {
+				err := DeleteIndex(tt.args.name)
+				assert.Nil(t, err)
+			})
+		})
+	}
+}
+
+func TestGetIndex(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  *Index
+		want1 bool
+	}{
+		{
+			name: "normal",
+			args: args{
+				name: "TestGetIndex.index_1",
+			},
+			want1: true,
+		},
+		{
+			name: "not exist",
+			args: args{
+				name: "TestGetIndex.index_2",
+			},
+			want1: false,
+		},
+	}
+
+	t.Run("prepare", func(t *testing.T) {
+		index, err := NewIndex("TestGetIndex.index_1", "disk", nil)
 		assert.Nil(t, err)
-		assert.Equal(t, index.Name, indexName)
+		assert.NotNil(t, index)
 
 		err = StoreIndex(index)
 		assert.Nil(t, err)
+	})
 
-		t.Run("cleanup", func(t *testing.T) {
-			err := DeleteIndex(indexName)
-			assert.Nil(t, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := GetIndex(tt.args.name)
+			if !tt.want1 {
+				assert.False(t, got1)
+				return
+			}
+			assert.True(t, got1)
+			assert.NotNil(t, got)
 		})
-	})
+	}
 
-	t.Run("test new index storage s3", func(t *testing.T) {
-		// TODO: support
+	t.Run("cleanup", func(t *testing.T) {
+		err := DeleteIndex("TestGetIndex.index_1")
+		assert.Nil(t, err)
 	})
-
-	t.Run("test new index storage minio", func(t *testing.T) {
-		// TODO: support
-	})
-
 }
